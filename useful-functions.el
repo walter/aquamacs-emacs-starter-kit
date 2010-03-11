a44e2a7bbcb4bab75174379c5d6ec722392839c0
Walter McGinnis <walter@katipo.co.nz>
2010-03-12 07:56:03 +1300

    adding duplicate line or region function that is accessible from C-c d

--- useful-functions.el
+++ useful-functions.el
@@ -21,4 +21,24 @@
   (untabify-buffer)
   (delete-trailing-whitespace))
 
+(defun duplicate-current-line-or-region (arg)
+  "Duplicates the current line or region ARG times.
+If there's no region, the current line will be duplicated. However, if
+there's a region, all lines that region covers will be duplicated."
+  (interactive "p")
+  (let (beg end (origin (point)))
+    (if (and mark-active (> (point) (mark)))
+        (exchange-point-and-mark))
+    (setq beg (line-beginning-position))
+    (if mark-active
+        (exchange-point-and-mark))
+    (setq end (line-end-position))
+    (let ((region (buffer-substring-no-properties beg end)))
+      (dotimes (i arg)
+        (goto-char end)
+        (newline)
+        (insert region)
+        (setq end (point)))
+      (goto-char (+ origin (* (length region) arg) arg)))))
+
 (provide 'useful-functions)
