9b04ab5561f0df0f496494a05446a0b9a880d734
Walter McGinnis <walter@katipo.co.nz>
2010-11-17 10:02:24 +1300

    Grabbed a function from http://blog.tuxicity.se/elisp/emacs/2010/11/16/delete-file-and-buffer-in-emacs.html.

--- useful-functions.el
+++ useful-functions.el
@@ -45,4 +45,20 @@ there's a region, all lines that region covers will be duplicated."
 ;; map it to keybinding
 (global-set-key (kbd "C-c d") 'duplicate-current-line-or-region)
 
+;; from http://blog.tuxicity.se/elisp/emacs/2010/11/16/delete-file-and-buffer-in-emacs.html
+(defun delete-this-buffer-and-file ()
+  "Removes file connected to current buffer and kills buffer."
+  (interactive)
+  (let ((filename (buffer-file-name))
+        (buffer (current-buffer))
+        (name (buffer-name)))
+    (if (not (and filename (file-exists-p filename)))
+        (error "Buffer '%s' is not visiting a file!" name)
+      (when (yes-or-no-p "Are you sure you want to remove this file? ")
+        (delete-file filename)
+        (kill-buffer buffer)
+        (message "File '%s' successfully removed" filename)))))
+
+(global-set-key (kbd "C-c k") 'delete-this-buffer-and-file)
+
 (provide 'useful-functions)
