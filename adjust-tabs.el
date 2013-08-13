50d009bd14c1a4f92561331a44b558ab92fc0032
Walter McGinnis <wm@waltermcginnis.com>
2013-08-13 17:00:56 +1200

    Add some model indent overriding for 2 spaces

--- adjust-tabs.el
+++ adjust-tabs.el
@@ -10,6 +10,18 @@
 (setq-default indent-tabs-mode nil)
 
 ;; make the default number of spaces to do tab indentation 2
+;; seen this both ways, as well as standard-indent 2 in customizations
+(setq-default tab-width 2)
 (setq default-tab-width 2)
+
+;; web-mode specific overrides of tab settings
+(defun web-mode-hook ()
+  "Hooks for Web mode."
+  (setq web-mode-markup-indent-offset 2)
+  (setq web-mode-css-indent-offset 2)
+  (setq web-mode-code-indent-offset 2)
+  (setq web-mode-indent-style 2)
+  )
+(add-hook 'web-mode-hook  'web-mode-hook)
 
 (provide 'adjust-tabs)
