968ef8607033c3722a4ac5a8ae56ab0c36136f85
Walter McGinnis <wm@waltermcginnis.com>
2016-03-03 15:07:46 +1300

    Add company-emoji to starter packages

--- vendor/starter-kit-packages.el
+++ vendor/starter-kit-packages.el
@@ -6,6 +6,7 @@
 ;; but modified to specify only packages that aren't already bundled with Aquamacs
 ;; and preferred by Walter McGinnis for Rails/Ruby work
 ;; (setq package-load-list '((coffee-mode t)
+;;                           (company-emoji t)
 ;;                           (erc-image t)
 ;;                           (flx-ido t)
 ;;                           (gist t)
